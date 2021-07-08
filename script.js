dc6b0dc70ee801ac21c86768f34126292cc7c96e
md-jishan-ansari <86798430+md-jishan-ansari@users.noreply.github.com>
2021-07-08 23:59:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector("#open").addEventListener
 ("click",function(){
     document.querySelector(".container").classList.add("show-nav");
-
+    document.querySelector(".circle").classList.add("circle-active");
 });
 document.querySelector("#close").addEventListener
 ("click",function(){
     document.querySelector(".container").classList.remove("show-nav");
-
+    document.querySelector(".circle").classList.remove("circle-active");
 });
